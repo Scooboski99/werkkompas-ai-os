ba909554f57ab073ef8d96bf695c6be03e938776
Scooboski99 <pelleseinen.school@gmail.com>
2026-03-17 21:17:54 +0100

    feat: wrap app with UserProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { UserProvider } from "@/context/UserContext";
 import LandingPage from "./pages/LandingPage";
 import OnboardingWizard from "./pages/OnboardingWizard";
 import DashboardPage from "./pages/DashboardPage";
@@ -27,28 +28,30 @@ function AppPage({ children }: { children: React.ReactNode }) {
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/onboarding" element={<OnboardingWizard />} />
-          <Route path="/dashboard" element={<AppPage><DashboardPage /></AppPage>} />
-          <Route path="/blueprint" element={<AppPage><CareerBlueprintPage /></AppPage>} />
-          <Route path="/rolrichtingen" element={<AppPage><RolrichtingenPage /></AppPage>} />
-          <Route path="/bedrijfstypen" element={<AppPage><BedrijfstypenPage /></AppPage>} />
-          <Route path="/vacature-analyzer" element={<AppPage><VacatureAnalyzerPage /></AppPage>} />
-          <Route path="/cv-studio" element={<AppPage><CVStudioPage /></AppPage>} />
-          <Route path="/motivatiebrief" element={<AppPage><MotivatiebriefPage /></AppPage>} />
-          <Route path="/interview-coach" element={<AppPage><InterviewCoachPage /></AppPage>} />
-          <Route path="/groeiplan" element={<AppPage><GroeiplanPage /></AppPage>} />
-          <Route path="/actiecentrum" element={<AppPage><ActiecentrumPage /></AppPage>} />
-          <Route path="/instellingen" element={<AppPage><InstellingenPage /></AppPage>} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+    <UserProvider>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/onboarding" element={<OnboardingWizard />} />
+            <Route path="/dashboard" element={<AppPage><DashboardPage /></AppPage>} />
+            <Route path="/blueprint" element={<AppPage><CareerBlueprintPage /></AppPage>} />
+            <Route path="/rolrichtingen" element={<AppPage><RolrichtingenPage /></AppPage>} />
+            <Route path="/bedrijfstypen" element={<AppPage><BedrijfstypenPage /></AppPage>} />
+            <Route path="/vacature-analyzer" element={<AppPage><VacatureAnalyzerPage /></AppPage>} />
+            <Route path="/cv-studio" element={<AppPage><CVStudioPage /></AppPage>} />
+            <Route path="/motivatiebrief" element={<AppPage><MotivatiebriefPage /></AppPage>} />
+            <Route path="/interview-coach" element={<AppPage><InterviewCoachPage /></AppPage>} />
+            <Route path="/groeiplan" element={<AppPage><GroeiplanPage /></AppPage>} />
+            <Route path="/actiecentrum" element={<AppPage><ActiecentrumPage /></AppPage>} />
+            <Route path="/instellingen" element={<AppPage><InstellingenPage /></AppPage>} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </UserProvider>
   </QueryClientProvider>
 );
 
